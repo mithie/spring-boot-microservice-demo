58363c65a1d1785a83023ffd9ebb8e69bbdd7fd3
Michael Thiem <mthiem0@gmail.com>
2018-08-06 16:31:52 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-java -jar eureka-service/target/eureka-service-0.0.1-SNAPSHOT.jar &
-
-sleep 10
-
-java -jar account-service/target/account-service-0.0.1-SNAPSHOT.jar &
-java -jar todo-service/target/todo-service-0.0.1-SNAPSHOT.jar &
